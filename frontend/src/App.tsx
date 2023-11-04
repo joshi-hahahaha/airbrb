c9aaa6ba1e1c34663af1dca35e391ae1c6de345e
WyReee <fan_young12@126.com>
2023-11-04 17:23:50 +1100

    Fix merge conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import theme from './assets/theme';
 import { ThemeProvider } from '@mui/material/styles';
-import RegisterModal from './components/RegisterModal';
+import RegisterModal from './components/register/RegisterModal';
 import Logo from './assets/airbrbLogo';
 import LoginModal from './components/LoginModal';
 
