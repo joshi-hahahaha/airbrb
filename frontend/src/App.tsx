14c1b2761c849c76408f7dc1507cd1a8da0eec61
WyReee <fan_young12@126.com>
2023-11-05 15:53:57 +1100

    added 3 routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import theme from './assets/theme';
 import { ThemeProvider } from '@mui/material/styles';
 import RegisterModal from './components/RegisterModal';
-import Logo from './assets/airbrbLogo';
 import LoginModal from './components/LoginModal';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 import { ListingPage } from './pages/ListingPage';
 
 const App = () => {
-  const [registerModalOpen, setRegisterModalOpen] = useState(false);
-  const [loginModalOpen, setLoginModalOpen] = useState(false);
-
-  const handleRegisterOpen = () => setRegisterModalOpen(true);
-  const handleLoginOpen = () => setLoginModalOpen(true);
-
-  const handleRegisterClose = () => setRegisterModalOpen(false);
-  const handleLoginClose = () => setLoginModalOpen(false);
-
   return (
-    <div className='App'>
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <ListingPage></ListingPage>
-        <Logo />
-        <button onClick={handleRegisterOpen}>Open Register</button>
-        <button onClick={handleLoginOpen}>Open Login</button>
-        <RegisterModal open={registerModalOpen} onClose={handleRegisterClose} />
-        <LoginModal open={loginModalOpen} onClose={handleLoginClose} />
+        <Routes>
+          <Route path="/register" element={<RegisterModal />} />
+          <Route path="/login" element={<LoginModal />} />
+          <Route path='/' element={<ListingPage />} />
+        </Routes>
       </ThemeProvider>
-    </div>
+    </BrowserRouter>
   );
 };
 
--- frontend/src/pages/ListingPage.tsx
+++ frontend/src/pages/ListingPage.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { listingPage } from '../styles/listPageStyle';
+import { Link } from 'react-router-dom';
 
 export const ListingPage = () => {
-  return <div style={listingPage}></div>;
+  return <div style={listingPage}>
+    <Link to="/register">Open Register</Link>
+    <Link to="/login">Open Login</Link>
+  </div>;
 };
