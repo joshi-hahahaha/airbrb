a1066ff2d0de0c3c26667bc1c82eb7a9c5b677c1
joshi-hahahaha <joshi.ha7@gmail.com>
2023-11-08 23:38:13 +1100

    IMP: basic listing card.

---
+++ frontend/src/helpers/generalHelpers.ts
@@ -0,0 +1,35 @@
+/**
+ * Given a js file object representing a jpg or png image, such as one taken
+ * from a html file input element, return a promise which resolves to the file
+ * data as a data url.
+ *
+ * @param {File} file The file to be read.
+ * @return {Promise<string>} Promise which resolves to the file as a data url.
+ */
+export const fileToDataUrl = (file: File): Promise<string> => {
+  const validFileTypes: string[] = ['image/jpeg', 'image/png', 'image/jpg'];
+  const valid: boolean = validFileTypes.some((type) => type === file.type);
+
+  // Bad data, let's walk away.
+  if (!valid) {
+    return Promise.reject(
+      new Error('Provided file is not a png, jpg, or jpeg image.')
+    );
+  }
+
+  const reader = new FileReader();
+
+  const dataUrlPromise: Promise<string> = new Promise((resolve, reject) => {
+    reader.onerror = () => {
+      reader.abort();
+      reject(new DOMException('Problem parsing input file.'));
+    };
+
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+  });
+
+  reader.readAsDataURL(file);
+  return dataUrlPromise;
+};
