81bbf2a8deb170e53c6d22392fbaf2e754150195
joshi-hahahaha <joshi.ha7@gmail.com>
2023-11-09 22:07:58 +1100

    FIX: NavBar component.

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -3,17 +3,40 @@ import { navBar } from '../../styles/navbar/navBarStyle';
 import Logo from '../../assets/airbrbLogo';
 import { SearchBar } from './SearchBar';
 import AuthContext from '../../contexts/AuthContext';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export const NavBar: React.FC = () => {
-  const { logout } = useContext(AuthContext);
+  const { authToken, logout } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+  const handleLoginBtn = () => {
+    navigate('/login');
+  };
+  const handleSignupBtn = () => {
+    navigate('/register');
+  };
 
   return (
     <nav style={navBar}>
       <Logo />
       <SearchBar />
       <div>
-        buttons
-        <button onClick={logout}>Logout</button>
+        {/*eslint-disable*/}
+        {authToken ? (
+          <Button onClick={logout} variant='outlined'>
+            Logout
+          </Button>
+        ) : (
+          <div>
+            <Button variant='outlined' onClick={handleLoginBtn}>
+              Login
+            </Button>
+            <Button variant='outlined' onClick={handleSignupBtn}>
+              Sign Up
+            </Button>
+          </div>
+        )}
       </div>
     </nav>
   );
