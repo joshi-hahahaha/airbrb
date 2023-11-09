86f290a519aa048df70f59ef773abca417e0f9cb
joshi-hahahaha <joshi.ha7@gmail.com>
2023-11-09 21:15:27 +1100

    FIX: NavBar login, logout button.

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -3,17 +3,27 @@ import { navBar } from '../../styles/navbar/navBarStyle';
 import Logo from '../../assets/airbrbLogo';
 import { SearchBar } from './SearchBar';
 import AuthContext from '../../contexts/AuthContext';
+import { Button } from '@mui/material';
 
 export const NavBar: React.FC = () => {
-  const { logout } = useContext(AuthContext);
+  const { authToken, logout } = useContext(AuthContext);
 
   return (
     <nav style={navBar}>
       <Logo />
       <SearchBar />
       <div>
-        buttons
-        <button onClick={logout}>Logout</button>
+        {authToken
+          ? (
+          <Button onClick={logout} variant='outlined'>
+            Logout
+          </Button>
+            )
+          : (
+          <Button onClick={logout} variant='outlined'>
+            Login
+          </Button>
+            )}
       </div>
     </nav>
   );
