91df88a2eac8922259b5bb878e866e0e0d147dfc
joshi-hahahaha <joshi.ha7@gmail.com>
2023-11-09 21:51:39 +1100

    IMP: login, register button functionality.

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -4,9 +4,18 @@ import Logo from '../../assets/airbrbLogo';
 import { SearchBar } from './SearchBar';
 import AuthContext from '../../contexts/AuthContext';
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export const NavBar: React.FC = () => {
   const { authToken, logout } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+  const handleLoginBtn = () => {
+    navigate('/login');
+  };
+  const handleSignupBtn = () => {
+    navigate('/register');
+  };
 
   return (
     <nav style={navBar}>
@@ -20,8 +29,12 @@ export const NavBar: React.FC = () => {
           </Button>
         ) : (
           <div>
-            <Button variant='outlined'>Login</Button>
-            <Button variant='outlined'>Login</Button>
+            <Button variant='outlined' onClick={handleLoginBtn}>
+              Login
+            </Button>
+            <Button variant='outlined' onClick={handleSignupBtn}>
+              Sign Up
+            </Button>
           </div>
         )}
       </div>
