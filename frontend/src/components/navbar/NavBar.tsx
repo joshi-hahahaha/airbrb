fca7da62ff6ad5d739269c8c2f1c7330ca89d8dc
joshi-hahahaha <joshi.ha7@gmail.com>
2023-11-12 15:54:32 +1100

    FIX: still the same logo naming issue, actually fixed tho.

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { navBar } from '../../styles/navbar/navBarStyle';
-import Logo from '../../assets/AirbrbLogo';
 import { SearchBar } from './SearchBar';
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { UserMenu } from './UserMenu';
+import Logo from '../../assets/airbrbLogo';
 
 export const NavBar: React.FC = () => {
   const navigate = useNavigate();
