b51fad60233b9c3cf0eff4e6ce97e2164a867c7b
WyReee <fan_young12@126.com>
2023-11-15 22:49:16 +1100

    bookings management done

--- frontend/src/components/bookingsPage/bookingsHistory.tsx
+++ frontend/src/components/bookingsPage/bookingsHistory.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BookingsProps } from '../../pages/BookingsPage';
 
 // All accepted/denied bookings
-// The booking request history for this listing consisting of all
-// booking requests for this listing and their status (accepted/denied)
-const BookingsHistory: React.FC<BookingsProps> = ({ bookings, listing }) => {
+const BookingsHistory: React.FC<BookingsProps> = ({
+  bookings,
+  listing,
+}) => {
   const historyBookings = bookings.filter(
-    booking => booking.listingId === listing.id && (booking.status === 'accepted' ||
-    booking.status === 'denied')
+    booking => booking.status === 'accepted' ||
+    booking.status === 'denied'
   );
+  console.log(listing.id);
 
   return <>
   {historyBookings.map((historyBooking) => (
