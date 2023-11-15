243460e62644f6e2b63f0963d6fb4702ccb7d627
joshi-hahahaha <joshi.ha7@gmail.com>
2023-11-15 23:14:14 +1100

    IMP: basic booking date picker.

--- frontend/src/components/listingPage/ListingReserveForm.tsx
+++ frontend/src/components/listingPage/ListingReserveForm.tsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 import { ListingHeaderProps } from '../../pages/ListingPage';
+import { Box, Typography } from '@mui/material';
+import AuthContext from '../../contexts/AuthContext';
+
+// interface DateRange {
+//   startDate: Date | null;
+//   endDate: Date | null;
+// }
 
 export const ListingReserveForm: React.FC<ListingHeaderProps> = ({
   listing,
 }) => {
-  console.log(listing);
-  return <div style={{ width: '40%' }}>reserve</div>;
+  const { authToken } = useContext(AuthContext);
+  console.log(authToken);
+
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+
+  return (
+    <div style={{ width: '40%', display: 'flex', justifyContent: 'flex-end' }}>
+      <Box sx={{ width: '90%', boxShadow: 3, borderRadius: '25px', p: '10px' }}>
+        <div style={{ width: '100%' }}>
+          <Typography
+            variant='h6'
+            style={{ fontFamily: 'Samsung-Regular' }}
+            gutterBottom
+          >
+            {`$${listing.price} per night`}
+          </Typography>
+        </div>
+        <div style={{ width: '100%' }}>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              label='Start Date'
+              value={today}
+              minDate={today}
+              sx={{ width: '100%', mt: '5px', mb: '5px' }}
+            />
+            <DatePicker
+              label='End Date'
+              value={tomorrow}
+              minDate={today}
+              sx={{ width: '100%', mt: '5px', mb: '5px' }}
+            />
+          </LocalizationProvider>
+        </div>
+        <div>button</div>
+      </Box>
+    </div>
+  );
 };
