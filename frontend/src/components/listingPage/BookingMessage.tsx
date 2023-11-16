ce2d5ed6d5d89d6379c6a841c3b3c43f7ebd6c5f
joshi-hahahaha <joshi.ha7@gmail.com>
2023-11-17 04:09:17 +1100

    IMP: spacing and styling.

--- frontend/src/components/listingPage/BookingMessage.tsx
+++ frontend/src/components/listingPage/BookingMessage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Typography, Box } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import { Booking } from '../../interfaces/bookingsInterfaces';
 
 interface BookingMessageProps {
@@ -8,33 +8,42 @@ interface BookingMessageProps {
 
 export const BookingMessage: React.FC<BookingMessageProps> = ({ booking }) => {
   console.log(booking);
-  return <></>;
-  // return (
-  //   <div>
-  //     <Box
-  //       sx={{
-  //         boxShadow: 1,
-  //         width: '100%',
-  //         display: 'flex',
-  //         py: '10px',
-  //         mb: '20px',
-  //         borderRadius: '10px',
-  //       }}
-  //     >
-  //       <div style={{ padding: '0 20px', minWidth: '50px' }}>
-  //         <Typography>{`★ ${formattedRating}`}</Typography>
-  //       </div>
-  //       <div
-  //         style={{
-  //           width: '100%',
-  //           overflow: 'hidden',
-  //           textOverflow: 'ellipsis',
-  //           paddingRight: '20px',
-  //         }}
-  //       >
-  //         <Typography>{review.reviewMsg}</Typography>
-  //       </div>
-  //     </Box>
-  //   </div>
-  // );
+  return (
+    <div>
+      <Box
+        sx={{
+          boxShadow: 1,
+          width: '100%',
+          display: 'flex',
+          py: '10px',
+          mb: '20px',
+          borderRadius: '10px',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{ minWidth: '80px', paddingLeft: '20px' }}>
+          <Typography sx={{ textAlign: 'center' }}>{booking.status}</Typography>
+        </div>
+        <div
+          style={{
+            flexGrow: 1,
+          }}
+        >
+          <Typography
+            sx={{ textAlign: 'center' }}
+          >{`${booking.dateRange.startDate} ${booking.dateRange.endDate}`}</Typography>
+        </div>
+        <div
+          style={{
+            minWidth: '80px',
+          }}
+        >
+          <Typography
+            sx={{ textAlign: 'center', paddingRight: '20px' }}
+          >{`$${booking.totalPrice}`}</Typography>
+        </div>
+      </Box>
+    </div>
+  );
 };
