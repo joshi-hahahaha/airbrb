f8dcc6ad77cbb17ce6da01b9630dd88dce72931f
joshi-hahahaha <joshi.ha7@gmail.com>
2023-11-17 13:05:52 +1100

    IMP: modal

--- frontend/src/components/listingPage/AdvancedRatingView.tsx
+++ frontend/src/components/listingPage/AdvancedRatingView.tsx
@@ -1,4 +1,4 @@
-import { Button, Tooltip, Typography } from '@mui/material';
+import { Box, Button, Fade, Modal, Tooltip, Typography } from '@mui/material';
 import React from 'react';
 import { ListingHeaderProps } from '../../pages/ListingPage';
 
@@ -6,6 +6,10 @@ export const AdvancedRatingView: React.FC<ListingHeaderProps> = ({
   listing,
 }) => {
   console.log(listing);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const countReviewsForStar = (rating: number): number => {
     if (listing.reviews !== undefined) {
       return listing.reviews.filter((review) => review.rating === rating)
@@ -35,13 +39,51 @@ export const AdvancedRatingView: React.FC<ListingHeaderProps> = ({
         rating
       ).toFixed(2)}%`}
     >
-      <Button>
+      <Button onClick={handleOpen}>
         <Typography variant='h6'>{`★ ${rating}`}</Typography>
       </Button>
     </Tooltip>
   );
 
   return (
-    <div style={{ width: '100%' }}>{[1, 2, 3, 4, 5].map(renderStarButton)}</div>
+    <>
+      <div style={{ width: '100%' }}>
+        {[1, 2, 3, 4, 5].map(renderStarButton)}
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              height: '80%',
+              width: '60%',
+              bgcolor: 'background.paper',
+              boxShadow: 24,
+              p: 4,
+              borderRadius: '10px',
+            }}
+          >
+            <Typography id='transition-modal-title' variant='h6' component='h2'>
+              Text in a modal
+            </Typography>
+            <Typography id='transition-modal-description' sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </>
   );
 };
